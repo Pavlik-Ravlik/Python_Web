d3b2686056237281616e84394c913149ac505e6f
Pavlik-Ravlik <pastet1990@icloud.com>
2023-07-06 06:54:46 +0300

    v 0.0.3

--- Homework_3/factorize_func.py
+++ Homework_3/factorize_func.py
@@ -2,13 +2,16 @@ import multiprocessing
 import time
 
 
-def factorize(number):
-    factors = []
-    
-    for i in range(1, number + 1):            
-        if number % i == 0:
-            factors.append(i)
-    return factors
+def factorize(*numbers):
+    results = []
+
+    for number in numbers:
+        factors = []
+        for i in range(1, number + 1):
+            if number % i == 0:
+                factors.append(i)
+        results.append(factors)
+    return results
 
 
 def parallel_factorize(numbers):
@@ -22,8 +25,14 @@ def parallel_factorize(numbers):
 def main():
     # Синхронна версія
     start_time = time.time()
-    a, b, c, d = factorize(128), factorize(
-        255), factorize(99999), factorize(10651060)
+
+    a, b, c, d = factorize(128, 255, 99999, 10651060)
+
+    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316,
+                 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
     end_time = time.time()
     execution_time = end_time - start_time
     print("Синхронна версія:", execution_time)
@@ -35,6 +44,7 @@ def main():
     end_time = time.time()
     execution_time = end_time - start_time
     print("Паралельна версія:", execution_time)
+
 
 if __name__ == '__main__':
     main()
